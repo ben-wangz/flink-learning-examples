ea502d2ade3e451a10f72243115bd85f6cd9e5ac
ben.wangz <ben.wangz@foxmail.com>
2023-11-08 14:58:24 +0800

    add window

--- hello-world/src/main/java/com/example/helloworld/SensorApp.java
+++ hello-world/src/main/java/com/example/helloworld/SensorApp.java
@@ -1,8 +1,16 @@
 package com.example.helloworld;
 
+import java.util.stream.StreamSupport;
+
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.runtime.operators.util.AssignerWithPeriodicWatermarksAdapter;
+import org.apache.flink.util.Collector;
+
 import com.example.helloworld.sink.SensorSink;
 import com.example.helloworld.source.SensorReading;
 import com.example.helloworld.source.SensorSource;
@@ -25,8 +33,26 @@ public class SensorApp {
 		DataStream<SensorReading> transformedReadingStream = sensorReadingStream
 				.filter(reading -> reading.getTemperature() >= 25)
 				.keyBy(SensorReading::getId)
-				// add window?
-				.reduce((r1, r2) -> r1.getTemperature() > r2.getTemperature() ? r1 : r2);
+				.window(TumblingEventTimeWindows.of(Time.seconds(5)))
+				.process(new ProcessWindowFunction<SensorReading, SensorReading, String, TimeWindow>() {
+					@Override
+					public void process(
+							String key,
+							Context context,
+							Iterable<SensorReading> elements,
+							Collector<SensorReading> collector) throws Exception {
+						double average = StreamSupport.stream(elements.spliterator(), false)
+								.mapToDouble(element -> element.getTemperature())
+								.average()
+								.orElse(0.0);
+						TimeWindow currentWindow = context.window();
+						collector.collect(SensorReading.builder()
+								.id(key)
+								.timestamp(currentWindow.getStart() + currentWindow.getEnd() / 2)
+								.temperature(average)
+								.build());
+					}
+				});
 		transformedReadingStream.addSink(SensorSink.builder().build());
 		// execute program
 		env.execute("transform example of sensor reading");
