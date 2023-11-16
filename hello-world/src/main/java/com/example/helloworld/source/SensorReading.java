2b9108bcc1ce96ceb2f1c45b59ea57764e3348a4
ben.wangz <ben.wangz@foxmail.com>
2023-11-16 10:33:49 +0800

    bugfix: POJO types

--- hello-world/src/main/java/com/example/helloworld/source/SensorReading.java
+++ hello-world/src/main/java/com/example/helloworld/source/SensorReading.java
@@ -1,16 +1,22 @@
 package com.example.helloworld.source;
 
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @ToString
-public class SensorReading {
-    private final String id;
-    private final long timestamp;
-    private final double temperature;
+public class SensorReading implements Serializable {
+    private String id;
+    private long timestamp;
+    private double temperature;
 }
